76d7649816c40288b5946651762be939ff5856ca
BrardAdrien <54979314+BrardAdrien@users.noreply.github.com>
2019-11-15 13:55:18 +0100

    commit 1

---
+++ DMX.cpp
@@ -0,0 +1,14 @@
+//---------------------------------------------------------------------------
+
+#pragma hdrstop
+
+#include "DMX.h"
+#include <mysql.h>
+#include <windows.h>
+
+int main()
+{
+   system("PAUSE");
+}
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
